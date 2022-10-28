fde29409f78088cf99bb90bce57bdc2c90a36a20
Gabriel Guimarães da Rocha <78444679+gabrielguimah@users.noreply.github.com>
2022-10-27 23:03:39 -0300

    Update Window.java

--- Window.java
+++ Window.java
@@ -8,7 +8,7 @@ public class Window extends JFrame {
     public Window() {
         windowConfig();
         setVisible(true);
-        setSize(650, 450);
+        setSize(650, 360);
         setTitle("Cadastro de Tarefas");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
@@ -17,15 +17,15 @@ public class Window extends JFrame {
         JPanel panel = new JPanel();
         panel.setLayout(null);
 
-        // Campo de texto
-        inputText = new JTextArea(45, 100);
-        inputText.setBounds(60, 30, 500, 80);
-        inputText.setBorder(BorderFactory.createLineBorder(getForeground()));
-
         // Etiqueta do campo 1
         JLabel firstLabel = new JLabel("Tarefa");
         firstLabel.setBounds(60, 10, 100, 10);
         firstLabel.setOpaque(true);
+
+        // Campo de texto
+        inputText = new JTextArea(45, 100);
+        inputText.setBounds(60, 30, 500, 80);
+        inputText.setBorder(BorderFactory.createLineBorder(getForeground()));
 
         // Etiqueta do campo 2
         JLabel secondLabel = new JLabel("Prioridade");
@@ -36,26 +36,29 @@ public class Window extends JFrame {
         optionsSelect = new JComboBox<>(options);
         optionsSelect.setBounds(60, 170, 500, 30);
 
-        // Botão de salvar
-        JButton saveButton = new JButton("Salvar");
-        saveButton.setBounds(350, 370, 80, 30);
+        // Checkbox
+        JCheckBox checkbox = new JCheckBox("Finalizado");
+        checkbox.setBounds(60, 220, 100, 30);
 
-        // Botão de cancelar
+        // Botão de Salvar
         JButton cancelButton = new JButton("Cancelar");
-        cancelButton.setBounds(450, 370, 90, 30);
+        cancelButton.setBounds(370, 270, 90, 30);
+
+        // Botão de Cancelar
+        JButton saveButton = new JButton("Salvar");
+        saveButton.setBounds(480, 270, 80, 30);
 
         panel.add(firstLabel);
-
-        panel.add(secondLabel);
-
-        panel.add(saveButton);
-
-        panel.add(cancelButton);
-
-        panel.add(optionsSelect);
-
         panel.add(inputText);
 
+        panel.add(secondLabel);
+        panel.add(optionsSelect);
+
+        panel.add(checkbox);
+
+        panel.add(cancelButton);
+        panel.add(saveButton);
+        
         add(panel);
     }
 
